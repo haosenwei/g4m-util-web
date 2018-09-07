4faf1c90c1733198f9a201d1413b0255983a07f0
961970674@q.com <961970674@qq.com>
2018-09-07 17:51:43 +0800

    1

--- src/main/java/com/g4m/G4mUtilWebApplication.java
+++ src/main/java/com/g4m/G4mUtilWebApplication.java
@@ -2,9 +2,16 @@ package com.g4m;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class G4mUtilWebApplication {
+public class G4mUtilWebApplication extends SpringBootServletInitializer{
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(G4mUtilWebApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(G4mUtilWebApplication.class, args);
